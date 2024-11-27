4225f8f601d537d104bf0aac53f848d96a0b53a6
ianalejandrook14x <taiwam34@gmail.com>
2024-11-27 09:26:59 -0300

    Update Anime•infoanime.js

--- plugins/Anime•infoanime.js
+++ plugins/Anime•infoanime.js
@@ -2,25 +2,25 @@ import fetch from 'node-fetch'
 
 var handler = async (m, { conn, usedPrefix, command, text }) => {
 
-if (!text) return conn.reply(m.chat, `🌸 *Ingrese el nombre de algun anime*\n\nEjemplo, ${usedPrefix + command} Kimetsu No Yaiba`, m, rcanal)
+if (!text) return conn.reply(m.chat, `*Ingrese el nombre de algun anime*\n\nEjemplo, ${usedPrefix + command} Kimetsu No Yaiba`, m, rcanal)
 let res = await fetch('https://api.jikan.moe/v4/manga?q=' + text)
-if (!res.ok) return conn.reply(m.chat, `🌸 *Ocurrió un fallo*`, m, rcanal)
+if (!res.ok) return conn.reply(m.chat, `*ocurrió un error*`, m, rcanal)
 
 let json = await res.json()
 let { chapters, title_japanese, url, type, score, members, background, status, volumes, synopsis, favorites } = json.data[0]
 let author = json.data[0].authors[0].name
-let animeingfo = `🌸 Título: ${title_japanese}
-🌸 Capítulo: ${chapters}
-💫 Transmisión: ${type}
-🗂 Estado: ${status}
-🗃 Volumes: ${volumes}
-🌟 Favorito: ${favorites}
-🧮 Puntaje: ${score}
-👥 Miembros: ${members}
-🔗 Url: ${url}
-👨‍🔬 Autor: ${author}
-📝 Fondo: ${background}
-💬 Sinopsis: ${synopsis}
+let animeingfo = ` Título: ${title_japanese}
+✧ Capítulo: ${chapters}
+✦ Transmisión: ${type}
+✧ Estado: ${status}
+✦ Volumes: ${volumes}
+✧ Favorito: ${favorites}
+✦ Puntaje: ${score}
+✧ Miembros: ${members}
+✦ Url: ${url}
+✧ Autor: ${author}
+✦ Fondo: ${background}
+✧ Sinopsis: ${synopsis}
  ` 
 conn.sendFile(m.chat, json.data[0].images.jpg.image_url, 'anjime.jpg', '      🌸 *I N F O - A N I M E* 🌸\n\n' + animeingfo, fkontak, m)
 
