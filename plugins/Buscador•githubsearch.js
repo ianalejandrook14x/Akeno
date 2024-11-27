e9ca17be675b2513ff11571c3589e186ea3e1063
ianalejandrook14x <taiwam34@gmail.com>
2024-11-27 09:31:47 -0300

    Update Buscador•githubsearch.js

--- plugins/Buscador•githubsearch.js
+++ plugins/Buscador•githubsearch.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-if (!text) return conn.reply(m.chat, `🌸 *Ingrese el nombre de un repositorio de github*\n\nEjemplo, ${usedPrefix + command} Ai-${botname}`, m, rcanal)
+if (!text) return conn.reply(m.chat, `*Ingrese el nombre de un repositorio de github*\n\nEjemplo, ${usedPrefix + command} ${botname}`, m, rcanal)
 try {
 await m.react(rwait)
 const res = await fetch(global.API('https://api.github.com', '/search/repositories', {
@@ -10,26 +10,26 @@ const json = await res.json()
 if (res.status !== 200) throw json
 let str = json.items.map((repo, index) => {
 return `
-🌸 *Resultado:* ${1 + index}
-🔗 *Enlace:* ${repo.html_url}
-👑 *Creador:* ${repo.owner.login}
-🌸 *Nombre:* ${repo.name}
-🫂 *Creado:* ${formatDate(repo.created_at)}
-💥 *Actualizado:* ${formatDate(repo.updated_at)}
-👀 *Visitas:* ${repo.watchers}
-✨️ *Bifurcado:* ${repo.forks}
-🌟 *Estrellas:* ${repo.stargazers_count}
-🌸 *Issues:* ${repo.open_issues}
-🍭 *Descripción:* ${repo.description ? `${repo.description}` : 'Sin Descripción'}
-⭐️ *Clone:* ${repo.clone_url}
+✧ *Resultado:* ${1 + index}
+✦ *Enlace:* ${repo.html_url}
+✧ *Creador:* ${repo.owner.login}
+✦ *Nombre:* ${repo.name}
+✧ *Creado:* ${formatDate(repo.created_at)}
+✧ *Actualizado:* ${formatDate(repo.updated_at)}
+✧ *Visitas:* ${repo.watchers}
+✧ *Bifurcado:* ${repo.forks}
+✦ *Estrellas:* ${repo.stargazers_count}
+✧ *Issues:* ${repo.open_issues}
+✦ *Descripción:* ${repo.description ? `${repo.description}` : 'Sin Descripción'}
+✧ *Clone:* ${repo.clone_url}
 `.trim()}).join('\n\n─────────────────\n\n') 
 // await m.react(done)
 let img = await (await fetch(json.items[0].owner.avatar_url)).buffer()
-await conn.sendMini(m.chat, '🌸 G I T H U B - S E A R C H 🌸', dev, str, img, img, redes, estilo)
+await conn.sendMini(m.chat, '✰ G I T H U B - S E A R C H ✰', dev, str, img, img, redes, estilo)
 await m.react(done)
 } catch {
 await m.react(error)
-conn.reply(m.chat, '🌸 *No se encontró resultados de:* ' + text, m, fake)}}
+conn.reply(m.chat, '*No se encontraron resultados de:* ' + text, m, fake)}}
 handler.help = ['githubsearch']
 handler.tags = ['buscador']
 handler.command = ['githubsearch']
