1399b3cc92a1a9e783391afc8512b2d92253e1c0
ianalejandrook14x <taiwam34@gmail.com>
2024-11-26 21:59:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ if (collaborators.hasOwnProperty(key)) {
 activeCollaborators += collaborators[key] + ', '
 }}
 activeCollaborators = activeCollaborators.slice(0, -2);
-cfonts.say('${botname}\nbot md', {
+cfonts.say('Bot\nBy Ian', {
 align: 'center',           
 gradient: ['red', 'blue'] 
 })
