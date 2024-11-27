6b12b1581bef2e3d5fe221e14f8b026192c3ce3b
ianalejandrook14x <taiwam34@gmail.com>
2024-11-27 10:09:45 -0300

    Update Descargas•imagen.js

--- plugins/Descargas•imagen.js
+++ plugins/Descargas•imagen.js
@@ -1,9 +1,9 @@
 import {googleImage} from '@bochilteam/scraper';
 
 const handler = async (m, {conn, text, usedPrefix, command}) => {
-if (!text) return conn.reply(m.chat, `*🌸 Uso Correcto: ${usedPrefix + command} ${botname}*`, m, rcanal);
+if (!text) return conn.reply(m.chat, `*✦ Uso Correcto: ${usedPrefix + command} ${botname}*`, m, rcanal);
 await m.react(rwait)
-conn.reply(m.chat, '🌸 *Descargando su imagen...*', m, {
+conn.reply(m.chat, '✦ *Descargando su imagen...*', m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: dev,
@@ -14,7 +14,7 @@ const image = await res.getRandom();
 const link = image;
 const messages = [['Imagen 1', dev, await res.getRandom(),
 [[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 3', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 4', dev, await res.getRandom(), [[]], [[]], [[]], [[]]]]
-await conn.sendCarousel(m.chat, `🌸 Resultado de ${text}`, '🔎 Imagen - Descargas', null, messages, m);
+await conn.sendCarousel(m.chat, `✧ Resultado de ${text}`, '✧ Imagen - Descargas', null, messages, m);
 await m.react(done)
 };
 handler.help = ['imagen <query>'];
