e7f975cf2b5c60c75db251e80dbb91eac9569835
ianalejandrook14x <taiwam34@gmail.com>
2024-11-27 12:13:08 -0300

    Update Grupo•encuesta.js

--- plugins/Grupo•encuesta.js
+++ plugins/Grupo•encuesta.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 
-if (!args[0]) throw `⚠️️ *_Ingrese un texto para iniciar la escuesta._*\n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
-if (!text.includes('|')) throw  `⚠️️ Separe las encuestas con *|* \n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
+if (!args[0]) throw `✧️ *Ingrese un texto para iniciar la escuesta.*\n\n✧ Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
+if (!text.includes('|')) throw  `✧️ Separe las encuestas con *|* \n\n✧ Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
 let a = []
 let b = text.split('|')
 for (let c = 0; c < b.length; c++) {
 a.push([b[c]])
                         }
-                        return conn.sendPoll(m.chat, `${packname}`, a, m)
+                        return conn.sendPoll(m.chat, `${botname}`, a, m)
 }
 handler.help = ['encuesta <text|text2>']
 handler.tags = ['grupo'] 
