94635c51fe1e2b2e189f15160039cd71e5e2db84
ianalejandrook14x <taiwam34@gmail.com>
2024-11-27 10:28:25 -0300

    Update Descargas•imagen.js

--- plugins/Descargas•imagen.js
+++ plugins/Descargas•imagen.js
@@ -5,7 +5,7 @@ if (!text) return conn.reply(m.chat, `*✦ Uso Correcto: ${usedPrefix + command}
 await m.react(rwait)
 conn.reply(m.chat, '✦ *Descargando su imagen...*', m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
+title: botname,
 body: dev,
 previewType: 0, thumbnail: icons,
 sourceUrl: channel }}})
