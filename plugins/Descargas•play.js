9a16c12adcf893642467d129230db8fd5008326f
ianalejandrook14x <taiwam34@gmail.com>
2024-11-26 21:06:15 -0300

    Update Descargas•play.js

--- plugins/Descargas•play.js
+++ plugins/Descargas•play.js
@@ -6,7 +6,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
     let search = await yts(text);
     let isVideo = /vid$/.test(command);
     let urls = search.all[0].url;
-    let body = `\`YouTube Play - ${botname}Bot-MD\`
+    let body = `\`YouTube Play - ${botname}\`
 
  ➢   *Título:* ${search.all[0].title}
      
