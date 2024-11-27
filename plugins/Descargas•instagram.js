c327112a4b260e14fab05392411e14c132f54fc3
ianalejandrook14x <taiwam34@gmail.com>
2024-11-27 10:11:46 -0300

    Update Descargas•instagram.js

--- plugins/Descargas•instagram.js
+++ plugins/Descargas•instagram.js
@@ -15,7 +15,7 @@ let res = await igdl(args[0])
 let data = res.data       
 for (let media of data) {
 await new Promise(resolve => setTimeout(resolve, 2000))           
-await conn.sendFile(m.chat, media.url, 'instagram.mp4', '✦ *Tu video de instagram.*\n' + textbot, fkontak)
+await conn.sendFile(m.chat, media.url, 'instagram.mp4', '✦ *Tu video de instagram.*\n' + botname, fkontak)
 }} catch {
 await m.react(error)
 conn.reply(m.chat, '✦ Ocurrió un error.', m, fake)}}
