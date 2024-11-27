e3cb202275be870efc65e0a79be1b867a257c4f1
ianalejandrook14x <taiwam34@gmail.com>
2024-11-27 09:24:16 -0300

    Update Anime•imagenes.js

--- plugins/Anime•imagenes.js
+++ plugins/Anime•imagenes.js
@@ -4,7 +4,7 @@ const res = (await axios.get(`https://raw.githubusercontent.com/ianalejandrook15
 await m.react(rwait)
 const messages = [['Imagen 1', dev, await res[Math.floor(res.length * Math.random())],
 [[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res[Math.floor(res.length * Math.random())], [[]], [[]], [[]], [[]]], ['Imagen 3', dev, await res[Math.floor(res.length * Math.random())], [[]], [[]], [[]], [[]]], ['Imagen 4', dev, await res[Math.floor(res.length * Math.random())], [[]], [[]], [[]], [[]]]]
-await conn.sendCarousel(m.chat, '🌸 Resultado de ' + command, '🔎 Anime - ' + command, null, messages, m);
+await conn.sendCarousel(m.chat, '✧ Resultado de ' + command, '✦ Anime - ' + command, null, messages, m);
 await m.react(done)
 };
 handler.command = handler.help = ['akira', 'akiyama', 'anna', 'asuna', 'ayuzawa', 'boruto', 'chiho', 'chitoge', 'deidara', 'erza', 'elaina', 'eba', 'emilia', 'hestia', 'hinata', 'inori', 'isuzu', 'itachi', 'itori', 'kaga', 'kagura', 'kaori', 'keneki', 'kotori', 'kurumi', 'madara', 'mikasa', 'miku', 'minato', 'naruto', 'nezuko', 'sagiri', 'sasuke', 'sakura', 'cosplay'];
