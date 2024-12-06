a2be5c091516cefc6a7535b30c94cab3aa4116c7
ianalejandrook14x <taiwam34@gmail.com>
2024-12-06 20:51:45 -0300

    Update Rpg•crimen.js

--- plugins/Rpg•crimen.js
+++ plugins/Rpg•crimen.js
@@ -8,7 +8,7 @@ let senderName = conn.getName(senderId)
 let tiempo = 5 * 60
 if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempo * 1000) {
 let tiempo2 = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempo * 1000 - Date.now()) / 1000))
-m.reply(`🌸 Ya has cometido un Crimen recientemente, espera ⏱️ *${tiempo2}* para cometer tu próximo Crimen y evitar ser atrapado.`)
+m.reply(`Debes esperar *${tiempo2}* para cometer otro crimen`)
 return
 }
 cooldowns[m.sender] = Date.now()
