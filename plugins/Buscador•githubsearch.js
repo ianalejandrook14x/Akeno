1270327c510ef55f410ae47ebdb08ac1ca093b8f
ianalejandrook14x <taiwam34@gmail.com>
2024-12-07 10:27:48 -0300

    Update Buscador•githubsearch.js

--- plugins/Buscador•githubsearch.js
+++ plugins/Buscador•githubsearch.js
@@ -25,7 +25,7 @@ return `
 `.trim()}).join('\n\n─────────────────\n\n') 
 // await m.react(done)
 let img = await (await fetch(json.items[0].owner.avatar_url)).buffer()
-await conn.sendNino(m.chat, '✰ G I T H U B - S E A R C H ✰', dev, str, img, img, redes, estilo)
+await conn.sendMini(m.chat, '✰ G I T H U B - S E A R C H ✰', dev, str, img, img, redes, estilo)
 await m.react(done)
 } catch {
 await m.react(error)
