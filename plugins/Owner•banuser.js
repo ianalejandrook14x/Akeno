3c95bb223976a401b580fe51d0bc521a35695305
ianalejandrook14x <taiwam34@gmail.com>
2024-12-06 21:21:32 -0300

    Update Owner•banuser.js

--- plugins/Owner•banuser.js
+++ plugins/Owner•banuser.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, text}) => {
-if (!text) return m.reply('⚠️ *Ingresa el @tag de algún usuario.*')
+if (!text) return m.reply('✦ *Ingresa el @tag de algún usuario.*')
 let who
 if (m.isGroup) who = m.mentionedJid[0]
 else who = m.chat
-if (!who) return m.reply('⚠️ *Ingresa el @tag de algún usuario.*')
+if (!who) return m.reply('✦ *Ingresa el @tag de algún usuario.*')
 let users = global.db.data.users
 users[who].banned = true
-conn.reply(m.chat, `⚠️ *El usuario @${who.split('@')[0]}, fue baneado con éxito.*`, fkontak, { mentions: [who]})
+conn.reply(m.chat, `✦ *El usuario @${who.split('@')[0]}, fue baneado con éxito.*`, fkontak, { mentions: [who]})
 }
 handler.help = ['banuser <@tag> <razón>']
 handler.command = ['banuser']
