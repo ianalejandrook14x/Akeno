59faaae1d7ee58d45d11049d355946eec04e52f1
ianalejandrook14x <taiwam34@gmail.com>
2024-12-06 21:52:14 -0300

    Update Grupo•delete.js

--- plugins/Grupo•delete.js
+++ plugins/Grupo•delete.js
@@ -1,6 +1,4 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
-
-if (!m.quoted) return conn.reply(m.chat, `🌸 Responde al mensaje que deseas eliminar.`, m, rcanal)
 try {
 let delet = m.message.extendedTextMessage.contextInfo.participant
 let bang = m.message.extendedTextMessage.contextInfo.stanzaId
