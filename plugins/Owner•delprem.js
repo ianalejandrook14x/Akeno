7b0736c767f83c436abca2a5e6510d64fefedfb6
ianalejandrook14x <taiwam34@gmail.com>
2024-12-06 21:17:18 -0300

    Update Owner•delprem.js

--- plugins/Owner•delprem.js
+++ plugins/Owner•delprem.js
@@ -3,16 +3,14 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
   else who = m.chat;
   const user = global.db.data.users[who];
-  if (!who) throw `🏓 Ingresa un @tag el que quiera quitarle el premium`;
-  if (!user) throw `*☘️ Ese humano no está en mi base de datos*`;
-  if (user.premiumTime = 0) throw '*🥷 El humano no es usuario premium 👑*';
+  if (!who) throw `✦ Ingresa un @tag el que quiera quitarle el premium`;
   const txt = text.replace('@' + who.split`@`[0], '').trim();
 
   user.premiumTime = 0;
 
   user.premium = false;
 
-  const textdelprem = `*@${who.split`@`[0]} ya no es usuario premium 👑*`;
+  const textdelprem = `*@${who.split`@`[0]} Ya no es usuario premium ✦*`;
   m.reply(textdelprem, null, {mentions: conn.parseMention(textdelprem)});
 };
 handler.help = ['delprem <@user>'];
