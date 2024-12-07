bc0b5777eca5fbea6b3c049a2bcd53c13a1c3122
ianalejandrook14x <taiwam34@gmail.com>
2024-12-07 10:22:24 -0300

    Update Owner•update.js

--- plugins/Owner•update.js
+++ plugins/Owner•update.js
@@ -11,7 +11,7 @@ let messager = stdout.toString()
 if (messager.includes('El bot ya se encuentra actualizado.')) messager = 'El bot ya esta actializado a la ultima versión.'
 
 if (messager.includes('✧ Actualizando.')) messager = '✧ Procesando, espere un momento mientras me actualizo.\n\n' + stdout.toString()
-conn.reply(m.chat, messager, m, ,)
+conn.reply(m.chat, messager, m,)
 
 } catch { 
 try {
@@ -26,7 +26,7 @@ return null
 return '*→ ' + line.slice(3) + '*'}).filter(Boolean)
 if (conflictedFiles.length > 0) {
 const errorMessage = `✧ Se han hecho cambios locales qué entran en conflicto con las Actualizaciones del Repositorio\n\n✰ *ARCHIVOS EN CONFLICTO*\n\n${conflictedFiles.join('\n')}`
-await conn.reply(m.chat, errorMessage, m, ,)
+await conn.reply(m.chat, errorMessage, m,)
 }
 }
 } catch (error) {
@@ -35,7 +35,7 @@ let errorMessage2 = '✧ Ocurrió un error inesperado.'
 if (error.message) {
 errorMessage2 += '\n✧ Mensaje de error: ' + error.message;
 }
-await conn.reply(m.chat, errorMessage2, m, ,)
+await conn.reply(m.chat, errorMessage2, m,)
 }
 }
 
