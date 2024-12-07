4e592c92c8c44fc9a367da39c163d0ac2d744c27
ianalejandrook14x <taiwam34@gmail.com>
2024-12-06 22:43:06 -0300

    Add files via upload

---
+++ plugins/Buscador•google.js
@@ -0,0 +1,27 @@
+import {googleIt} from '@bochilteam/scraper'
+import google from 'google-it'
+import axios from 'axios'
+let handler = async (m, { conn, command, args, usedPrefix }) => {
+const fetch = (await import('node-fetch')).default;
+const text = args.join` `
+if (!text) return conn.reply(m.chat, '✧ Ingresa lo que deseas buscar en Google.', m, )
+conn.reply(m.chat, `✧ Buscando Información.`, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: botname,
+body: dev,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
+const url = 'https://google.com/search?q=' + encodeURIComponent(text)
+google({'query': text}).then(res => {
+let teks = `✧ *Resultado de* : ${text}\n\n`
+for (let g of res) {
+teks += `✧ *Titulo ∙* ${g.title}\n✧ *Info ∙* ${g.snippet}\n✧ *Url ∙* ${g.link}\n\n`
+}
+conn.reply(m.chat, teks, m, )
+})
+}
+handler.help = ['google <búsqueda>']
+handler.tags = ['buscador']
+handler.command = ['google']
+handler.register = false
+export default handler
