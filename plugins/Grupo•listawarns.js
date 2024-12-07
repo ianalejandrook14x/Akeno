1e8d9d098ccf86c68eb8c11c74412d2227e99e4d
ianalejandrook14x <taiwam34@gmail.com>
2024-12-06 22:00:59 -0300

    Update Grupo•listawarns.js

--- plugins/Grupo•listawarns.js
+++ plugins/Grupo•listawarns.js
@@ -3,13 +3,12 @@ let adv = Object.entries(global.db.data.users).filter(user => user[1].warn)
 let warns = global.db.data.users.warn
 let user = global.db.data.users
 
-let caption = `⚠️ 𝙐𝙎𝙐𝘼𝙍𝙄𝙊𝙎 𝘼𝘿𝙑𝙀𝙍𝙏𝙄𝘿𝙊𝙎
-*╭•·–––––––––––––––––––·•*
-│ *Total : ${adv.length} Usuarios* ${adv ? '\n' + adv.map(([jid, user], i) => `
-│
-│ *${i + 1}.* ${conn.getName(jid)  == undefined ? 'Sin Usuarios' : conn.getName(jid) + ` *(${user.warn}/4)*`}
-│ ${isOwner ? '@' + jid.split`@`[0] : jid}\n│ - - - - - - - - -`.trim()).join('\n') : ''}
-*╰•·–––––––––––––––––––·•*\n\n⚠️ 𝗔𝗗𝗩𝗘𝗥𝗧𝗘𝗡𝗖𝗜𝗔 ⇢ ${warns ? `*${warns}/4*` : '*0/4*'}\n${botname}`
+let caption = `✦ Usuarios Advertidos
+*Total : ${adv.length} Usuarios* ${adv ? '\n' + adv.map(([jid, user], i) => `
+
+ *${i + 1}.* ${conn.getName(jid)  == undefined ? 'Sin Usuarios' : conn.getName(jid) + ` *(${user.warn}/4)*`}
+ ${isOwner ? '@' + jid.split`@`[0] : jid}\n│ - - - - - - - - -`.trim()).join('\n') : ''}
+\n\n✦ Advertencia ⇢ ${warns ? `*${warns}/4*` : '*0/4*'}\n${botname}`
 await conn.reply(m.chat, caption, m, { mentions: await conn.parseMention(caption) })}
 
 handler.help = ['listadv']
