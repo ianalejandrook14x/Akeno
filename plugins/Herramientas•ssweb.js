53d61e1c34c2c7bc4e901d81a2cb60f75c9c517f
ianalejandrook14x <taiwam34@gmail.com>
2024-12-07 11:24:29 -0300

    Update Herramientas•ssweb.js

--- plugins/Herramientas•ssweb.js
+++ plugins/Herramientas•ssweb.js
@@ -3,12 +3,6 @@ let handler = async (m, { conn, command, args }) => {
 if (!args[0]) return conn.reply(m.chat, '✧ *Ingrese el Link de una página.*', m, )
 try {
 await m.react(rwait)
-conn.reply(m.chat, '✧ Buscando su información....', m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: botname,
-body: dev,
-previewType: 0, thumbnail: icons, 
-sourceUrl: channel }}})
 let ss = await (await fetch(`https://image.thum.io/get/fullpage/${args[0]}`)).buffer()
 conn.sendFile(m.chat, ss, 'error.png', args[0], fkontak)
 await m.react(done)
