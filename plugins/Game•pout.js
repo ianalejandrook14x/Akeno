23237e05812942966a6f0560069bec517cb2a1a0
ianalejandrook14x <taiwam34@gmail.com>
2024-12-06 21:44:39 -0300

    Update Game•pout.js

--- plugins/Game•pout.js
+++ plugins/Game•pout.js
@@ -14,7 +14,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     let user = global.db.data.users[who];
     let name = conn.getName(who);
     let name2 = conn.getName(m.sender);
-    m.react('😶');
+    m.react('✅');
     let str = `${name2} le está haciendo pucheros a ${name}`.trim();
     if (m.isGroup){
     
