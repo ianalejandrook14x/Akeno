73a66ae05b45b66c027421d028d4d9a5d6091931
ianalejandrook14x <taiwam34@gmail.com>
2024-12-07 11:18:29 -0300

    Update Game•pokedex.js

--- plugins/Game•pokedex.js
+++ plugins/Game•pokedex.js
@@ -3,12 +3,6 @@ import fetch from 'node-fetch';
 let handler = async (m, { conn, text }) => {
 if (!text) return conn.reply(m.chat, '✦ *¿Que Pokémon quieres buscar?.*', m, )
 await m.react(rwait)
-conn.reply(m.chat, `✦ *Buscando ${text}*`, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: botname,
-body: dev,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
 const url = `https://some-random-api.com/pokemon/pokedex?pokemon=${encodeURIComponent(text)}`;
 const response = await fetch(url);
 const json = await response.json();
