f417b7601fef96d1e722e4be7561c57af09d8e17
ianalejandrook14x <taiwam34@gmail.com>
2024-12-06 21:09:05 -0300

    Update Owner•nuevabiobot.js

--- plugins/Owner•nuevabiobot.js
+++ plugins/Owner•nuevabiobot.js
@@ -1,11 +1,10 @@
 let handler = async (m, { conn, text }) => {
-   if (!text) return conn.reply(m.chat, '🌸 *¡Te faltó el texto mi propietario!*', m, rcanal)
-     try {
-                await conn.updateProfileStatus(text).catch(_ => _)
-                conn.reply(m.chat, `✅️ Info Cambiada Con Exito!`, m, rcanal)
+    try {
+               await conn.updateProfileStatus(text).catch(_ => _)
+               conn.reply(m.chat, `✦ La información fue cambiada`, m, rcanal)
 } catch {
-       throw 'Well, Error Sis...'
-     }
+      throw 'Well, Error Sis...'
+    }
 }
 handler.help = ['nuevabiobot <teks>']
 handler.tags = ['owner']
