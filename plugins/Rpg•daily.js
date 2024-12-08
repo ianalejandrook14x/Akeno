af7c073c0e94b04da9de3f77972060b1ff35743f
ianalejandrook14x <taiwam34@gmail.com>
2024-12-08 12:54:46 -0300

    Update Rpg•daily.js

--- plugins/Rpg•daily.js
+++ plugins/Rpg•daily.js
@@ -3,7 +3,7 @@ const prem = 15
 
 var handler = async (m, {conn, isPrems }) => {
 
-let coin = `${pickRandom([5, 6, 7, 9, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 99, 100, 110, 120, 130, 600, 1000, 1500])}` * 1
+let yenes = `${pickRandom([3500, 4500, 5000, 6500, 9000])}` * 1
 let exp = `${pickRandom([500, 600, 700, 800, 900, 999, 1000, 1300, 1500, 1800])}` * 1
 let exppremium = `${pickRandom([1000, 1500, 1800, 2100, 2500, 2900, 3300, 3600, 4000, 4500])}` * 1
 let d = Math.floor(Math.random() * 30)
@@ -15,9 +15,9 @@ global.db.data.users[m.sender].exp += exppremium ? prem : exp
 conn.reply(m.chat, `✧ *Recompensa Diaria*
 
 Recursos:
-✧ Xp : *+${isPrems ? exppremium : exp}*
+✧ XP : *+${isPrems ? exppremium : exp}*
 ✧ Diamantes : *+${d}*
-✧ Coins : *+${coin}*`, m, )
+✧ Yenes : *+${yenes}*`, m, )
 
 global.db.data.users[m.sender].lastclaim = new Date * 1
 
