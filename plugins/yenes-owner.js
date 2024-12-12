c594e4ff4317f602a15579730584465f0e19d394
ianalejandrook14x <taiwam34@gmail.com>
2024-12-12 12:50:11 -0300

    Update yenes-owner.js

--- plugins/yenes-owner.js
+++ plugins/yenes-owner.js
@@ -7,23 +7,23 @@ let handler = async (m, { conn, text }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (!who) return m.reply('⚠️️ *Taguea al usuario*')
+    if (!who) return m.reply('✦ *Taguea al usuario*')
     let txt = text.replace('@' + who.split`@`[0], '').trim()
-    if (!txt) return m.reply('⚠️️ Ingrese la cantidad de *Yenes* que quiere añadir')
-    if (isNaN(txt)) return m.reply('⚠️ *sólo números*')
+    if (!txt) return m.reply('✦️ Ingrese la cantidad de *Yenes* que quiere añadir')
+    if (isNaN(txt)) return m.reply('✦ *sólo números*')
     let dmt = parseInt(txt)
     let yenes = dmt
     let pjk = Math.ceil(dmt * impts)
     yenes += pjk
-    if (yenes < 1) return m.reply('⚠️️ Mínimo es  *1*')
+    if (yenes < 1) return m.reply('✦️ Mínimo es  *1*')
     let users = global.db.data.users
    users[who].yenes += dmt
 
-    await conn.reply(m.chat, `⊜ *💴 AÑADIDO*
+    await conn.reply(m.chat, `⊜ *AÑADIDO*
 ┏━━━━━━━━━━━⬣
 ┃⋄ *Total:* ${dmt}
 ┗━━━━━━━━━━━⬣`, m, )
-   conn.fakeReply(m.chat, `⊜ *Recibiste* \n\n *+${dmt} Yenes 💴*`, who, m.text)
+   conn.fakeReply(m.chat, `⊜ *Recibiste* \n\n *+${dmt} ${currency}*`, who, m.text)
 }
 
 handler.help = ['addyenes *<@user>*']
