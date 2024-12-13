294e7fce5d1dff6bd533631448c848699c387cd1
ianalejandrook14x <taiwam34@gmail.com>
2024-12-13 15:06:02 -0300

    Create config-welcome.js

---
+++ plugins/config-welcome.js
@@ -0,0 +1,17 @@
+import {WAMessageStubType} from '@whiskeysockets/baileys'
+import fetch from 'node-fetch'
+
+export async function before(m, {conn, participants, groupMetadata}) {
+  if (!m.messageStubType || !m.isGroup) return !0;
+  let welcom = `Hola bienvenido al grupo ✦`
+    let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => welcome)
+    let pp2 = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => adios)
+  let img = await (await fetch(`${pp}`)).buffer()
+  let img2 = await (await fetch(`${pp2}`)).buffer()
+  let chat = global.db.data.chats[m.chat]
+
+ if (chat.welcome && m.messageStubType == 27) {
+    let welcome = `${welcom}`
+await conn.sendMini(m.chat, welcome)
+  }
+}
