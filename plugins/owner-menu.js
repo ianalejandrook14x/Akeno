e72aedb4c918c5a8195623e3fd2845495188ff89
ianalejandrook14x <taiwam34@gmail.com>
2024-12-13 20:28:56 -0300

    Create owner-menu.js

---
+++ plugins/owner-menu.js
@@ -0,0 +1,40 @@
+let handler = async (m, { conn }) => {
+  let txt = `*Menu - creador* ✧
+  
+*${prefix}copia
+*${prefix}e
+*${prefix}addprem <@tag> <days>
+*${prefix}addxp <@tag> <monto>
+*${prefix}autoadmin
+*${prefix}banuser <@tag> <motivo>
+*${prefix}broadcast
+*${prefix}bc
+*${prefix}cheat
+*${prefix}cleartmp
+*${prefix}delprem <@tag>
+*${prefix}fetch
+*${prefix}get
+*${prefix}ip <dirección>
+*${prefix}join <link>
+*${prefix}nuevabiovot <txt>
+*${prefix}grupocrear <txt>
+*${prefix}otorgar <mod/prem>
+*${prefix}quitar <mod/prem>
+*${prefix}resetperonajes
+*${prefix}restart
+*${prefix}unbanuser <@tag>
+*${prefix}addyenes <@tag>`.trim();
+
+let perfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://qu.ax/QGAVS.jpg')
+
+  //await conn.sendFile(m.chat, imagen1, 'menu.jpg', txt, m);
+  //await conn.sendMini(m.chat, botname, textbot, , img, img, rcanal, estilo)
+  await conn.sendMessage(m.chat, { text: txt, contextInfo: { externalAdReply: { title: botname, body: dev, thumbnailUrl: banner, mediaType: 1, showAdAttribution: true, renderLargerThumbnail: true }}} , { quoted: m })
+};
+
+handler.help = ['owner-mennu'];
+handler.tags = ['main'];
+handler.command = ['owner-menu'];
+handler.register = true;
+
+export default handler;
