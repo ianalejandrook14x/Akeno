a138c6b234521595a2aeac007c9ca62380f4521a
ianalejandrook14x <taiwam34@gmail.com>
2024-12-13 18:47:01 -0300

    Update Sticker•sticker.js

--- plugins/Sticker•sticker.js
+++ plugins/Sticker•sticker.js
@@ -40,7 +40,7 @@ else return m.reply(`💫 El url es incorrecto`)
 console.error(e)
 if (!stiker) stiker = e
 } finally {
-if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: `${botname} Bot - MD 🌸`, body: `✡︎ Sticker By • ${botname}Bot`, mediaType: 2, sourceUrl: redes, thumbnail: icons}}}, { quoted: m })
+if (stiker) conn.sendFile(m.chat, stiker, { quoted: m })
 
 else return conn.reply(m.chat, '*Responda a una imagen*', m, )
 
