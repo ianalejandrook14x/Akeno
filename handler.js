102f01a6d0434798a400572377acd762ea974f60
ianalejandrook14x <taiwam34@gmail.com>
2024-12-14 23:36:01 -0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -295,8 +295,8 @@ return
 
 //Antispam 2                
 if (user.antispam2 && isROwner) return
-let time = global.db.data.users[m.sender].spam + 3000
-if (new Date - global.db.data.users[m.sender].spam < 3000) return console.log(`[ SPAM ]`) 
+let time = global.db.data.users[m.sender].spam + 6000
+if (new Date - global.db.data.users[m.sender].spam < 6000) return console.log(`[ SPAM ]`) 
 global.db.data.users[m.sender].spam = new Date * 1
 }
 if (m.chat in global.db.data.chats || m.sender in global.db.data.users) {
