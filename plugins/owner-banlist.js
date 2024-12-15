bda5399cf75bd230959136219847090d1fcd1a3f
ianalejandrook14x <taiwam34@gmail.com>
2024-12-14 23:31:03 -0300

    Update owner-banlist.js

--- plugins/owner-banlist.js
+++ plugins/owner-banlist.js
@@ -15,5 +15,5 @@ const handler = async (m, {conn, isOwner}) => {
   m.reply(caption, null, {mentions: conn.parseMention(caption)});
 };
 handler.command = ['banlist', 'listban']
-handler.rowner = true;
+handler.mods = true;
 export default handler;
