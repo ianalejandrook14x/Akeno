9086a279b87250c97baf98681fabb7456a6a06c8
ianalejandrook14x <taiwam34@gmail.com>
2024-12-14 23:53:10 -0300

    Update owner-newbio.js

--- plugins/owner-newbio.js
+++ plugins/owner-newbio.js
@@ -9,6 +9,6 @@ let handler = async (m, { conn, text }) => {
 handler.help = ['nuevabiobot <teks>']
 handler.tags = ['owner']
 handler.command = ['nuevabiobot', 'setbotbot']
-handler.owner = true
+handler.mods = true
 
 export default handler
