1f1fc1d87f80b73909683e731b7a7e18c74d445d
ianalejandrook14x <taiwam34@gmail.com>
2024-12-16 19:26:10 -0300

    Create adda.js

---
+++ plugins/adda.js
@@ -0,0 +1,43 @@
+import fs from 'fs';
+
+function loadData() {
+  try {
+    const data = fs.readFileSync('./personajes.json');
+    return JSON.parse(data);
+  } catch (error) {
+    return {}; 
+  }
+}
+
+function saveData(animes) {
+  fs.writeFileSync('./personajes.json', JSON.stringify(animes, null, 2));
+}
+
+const handler = async (m, { command, args }) => {
+  const animeName = args.join(" "); 
+  const animeId = generateAnimeId(); 
+  
+  const animes = loadData();
+
+  if (animes[animeId]) {
+    return m.reply("Este anime ya existe.");
+  }
+
+  animes[animeId] = {
+    name: animeName,
+    characters: [],
+  };
+
+  saveData(animes);
+  
+  return m.reply(`*${animeName} | ID: ${animeId}*`);
+};
+
+function generateAnimeId() {
+  return Math.random().toString(36).substring(2, 10);
+}
+
+handler.help = ['adda'];
+handler.command = ['adda'];
+
+export default handler;
