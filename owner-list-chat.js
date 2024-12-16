392d7bb5f7f383c49e20e730f19580a8065efc3c
ianalejandrook14x <taiwam34@gmail.com>
2024-12-16 11:53:53 -0300

    Create owner-list-chat.js

---
+++ owner-list-chat.js
@@ -0,0 +1,40 @@
+let handler = async (m, { conn }) => {
+    const chats = global.db.data.chats;
+    
+   
+    const isROwner = m.fromMe || global.owner?.includes(m.sender);
+  
+    if (!isROwner) {
+      return conn.reply(m.chat, '*✦ Solo el propietario del bot puede utilizar este comando*', m);
+    }
+  
+    if (/^\/chat_baneado$/i.test(m.text)) {
+     
+      let bannedChats = Object.entries(chats)
+        .filter(([id, data]) => data.isBanned)
+        .map(([id, data]) => `${id} (${data.name || '✦'})`);
+      
+      if (bannedChats.length === 0) {
+        return conn.reply(m.chat, '*✦ No se encontraron chats baneados*', m);
+      }
+      
+      let message = `✦ *Lista de chats baneados:*\n\n${bannedChats.join('\n')}`;
+      return conn.reply(m.chat, message, m);
+    }
+  
+    if (/^\/unbanchat_all$/i.test(m.text)) {
+  
+      for (let id in chats) {
+        chats[id].isBanned = false;
+      }
+      return conn.reply(m.chat, '✦ *Todos los chats han sido desbaneados.*', m);
+    }
+  };
+  
+  handler.help = ['chat_baneado', 'unbanchat_all'];
+  handler.tags = ['mods'];
+  handler.command = ['chat_baneado', 'unbanchat_all'];
+  handler.rowner = true;
+  
+  export default handler;
+  
