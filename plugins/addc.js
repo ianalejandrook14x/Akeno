3a18a5d48c6aeb53382fa58eb4587ed3b4f77a32
ianalejandrook14x <taiwam34@gmail.com>
2024-12-16 19:36:02 -0300

    Update addc.js

--- plugins/addc.js
+++ plugins/addc.js
@@ -9,35 +9,31 @@ function loadData() {
   }
 }
 
-function saveData(animes) {
-  fs.writeFileSync('./personajes.json', JSON.stringify(animes, null, 2));
+function saveData(data) {
+  fs.writeFileSync('./personajes.json', JSON.stringify(data, null, 2));
 }
 
 const handler = async (m, { command, args }) => {
   const animes = loadData();
-  
-  const animeId = args[0]; 
-  const tag = args[1];
-  const name = args.slice(2).join(" "); 
-  
-  if (!animes[animeId]) {
-    return m.reply("Anime no encontrado.");
+  const animeId = args[0];
+  const newCharacters = args.slice(1).join(" ").split("\n");
+
+  if (!animeId || !animes[animeId]) {
+    return m.reply('*No se encontro el anime*');
   }
 
-  const character = {
-    tag,
-    name,
-    anime: animes[animeId].name,
-    value: 50,  
-    price: 1000,
-  };
+  const anime = animes[animeId];
 
-
-  animes[animeId].characters.push(character);
+  newCharacters.forEach((character) => {
+    const [tag, name] = character.split(" = ");
+    if (tag && name) {
+      anime.characters.push({ tag: tag.trim(), name: name.trim(), value: 50, price: 1000 });
+    }
+  });
 
   saveData(animes);
-  
-  return m.reply(`> Personaje agregado al anime ${animes[animeId].name}\n*${name}*\n`);
+
+  m.reply(`*Se agrego personajes al anime: ${anime.name}*`);
 };
 
 handler.help = ['addc'];
