61fdad4268ec12f0a550e1809061ba5d6660cbd5
ゞ 𝐆𝐚𝐛𝐫𝐢𝐞𝐥 𝐒𝐤 ぁ <148560941+glytglobal@users.noreply.github.com>
2024-12-17 22:24:47 -0300

    • Akeno Himejima •

--- plugins/rpg•perfil.js
+++ plugins/rpg•perfil.js
@@ -1,9 +1,8 @@
-
 import PhoneNumber from 'awesome-phonenumber'
 import fetch from 'node-fetch'
 var handler = async (m, { conn }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://ibb.co/9H3bspF')
+let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60')
 let { premium, level, yenes, exp, lastclaim, registered, regTime, age, role } = global.db.data.users[m.sender]
 let username = conn.getName(who)
 let noprem = `
@@ -11,8 +10,8 @@ let noprem = `
 ✦ *Nombre:* ${username}
 
 ✦ *RECURSOS*
-✦ *${currency}:* ${yenes}
-✦ *Experiencia:* ${exp}
+✦ *${currency}:* ${yenes || 'Sin Información'}
+✦ *Experiencia:* ${exp || 'Sin Información'}
 ✦ *Premium*: *${prems ? 'Si' : 'No'}*`.trim()
 conn.sendFile(m.chat, pp, 'perfil.jpg', `${premium ? prem.trim() : noprem.trim()}`, m, rcanal, { mentions: [who] })
 }
