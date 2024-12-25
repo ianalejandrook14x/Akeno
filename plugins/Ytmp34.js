8d82bca347ba756d167b685a3ccfe1734a530322
Oscar66266 <oscarabrahamcabreramarin098@gmail.com>
2024-12-25 14:00:32 -0500

    Update Ytmp34.js

--- plugins/Ytmp34.js
+++ plugins/Ytmp34.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn: star, args, text, usedPrefix, command }) => {
   if (!args || !args[0]) return star.reply(m.chat, '✦ *Ingrese el enlace de un video de YouTube*', m)
   if (!args[0].match(/youtu/gi)) return star.reply(m.chat, `✦ *Verifica que el enlace sea de YouTube.*`, m).then(_ => m.react('✖️'))
   
-  let q = args[1] || '360p'
+  let q = args[1] || '720p'
   await m.react('🕓')
 
   try {
