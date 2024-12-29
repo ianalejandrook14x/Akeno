4c1dd66eda4bde530c30657680714a29c2df3f36
ianalejandrook14x <taiwam34@gmail.com>
2024-12-29 12:28:10 -0300

    Update prueba_test.js

--- plugins/prueba_test.js
+++ plugins/prueba_test.js
@@ -1,36 +1,27 @@
-import fetch from 'node-fetch';
-let handler = async (m, { conn, usedPrefix, command }) => {
+const handler = async (m, { conn, args, command }) => {
     if (command === 'test') {
-        const imageUrl = 'https://qu.ax/BWXFU.jpg'; // URL
-        const messageText = `test  ${mentionedUser}`;
-        await conn.sendButton(m.chat, messageText, 'Test', imageUrl, [
-            ['Sí', `${usedPrefix}Si`],
-            ['No', `${usedPrefix}No`]
-        ], m);
+        const questions = [
+            {
+                text: "Te gusta Akeno Himejima??",
+                image: "https://qu.ax/BWXFU.jpg",
+                buttons: [
+                    { buttonId: 'Si', buttonText: { displayText: "Rojo" }, type: 1 },
+                    { buttonId: 'No', buttonText: { displayText: "Azul" }, type: 1 },
+                    { buttonId: 'talvez', buttonText: { displayText: "Verde" }, type: 1 },
+                ],
+            },
+        ];
+        for (const question of questions) {
+            await conn.sendMessage(m.chat, {
+                text: question.text,
+                image: { url: question.image },
+                buttons: question.buttons,
+                headerType: 4,
+            });
+        }
     }
 };
-let siHandler = async (m, { conn, usedPrefix, command }) => {
-    if (command === 'si') {
-        const yesImageUrl = 'https://qu.ax/BWXFU.jpg'; // Imagen para la respuesta "Sí"
-        const yesMessageText = `*Te gustan las mujeres`;
-        await conn.sendMessage(m.chat, { 
-            image: { url: yesImageUrl }, 
-            caption: yesMessageText
-        }, { quoted: m });
-    }
-};
-let noHandler = async (m, { conn, usedPrefix, command }) => {
-    if (command === 'no') {
-        const noImageUrl = 'https://qu.ax/BWXFU.jpg'; // Imagen para la respuesta "No"
-        const noMessageText = `*No te gustan las mujeres`;
-        await conn.sendMessage(m.chat, { 
-            image: { url: noImageUrl }, 
-            caption: noMessageText
-        }, { quoted: m });
-    }
-};
-// Vincular los comandos al texto "#test", "si", y "no"
-handler.command = ['test', 'si', 'no'];
-handler.tags = ["downloader"]
-handler.help = ["test"];
+
+handler.command = ['test'];
+
 export default handler;
