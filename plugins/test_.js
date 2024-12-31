bfed4749e69ba4339775a7dd45060f34efb7ac4c
ianalejandrook14x <taiwam34@gmail.com>
2024-12-31 00:34:49 -0300

    Create test_.js

---
+++ plugins/test_.js
@@ -0,0 +1,21 @@
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+let txt = `Prueba de botones`
+conn.sendMessage(m.chat, { text: txt, caption: "1234", footer: wm, buttons: [
+  {
+    buttonId: ".menu", 
+    buttonText: { 
+      displayText: 'test' 
+    }
+  }, {
+    buttonId: ".tiktoksearch nakano miku", 
+    buttonText: {
+      displayText: "test2"
+    }
+  }
+],
+  viewOnce: true,
+  headerType: 1,
+}, { quoted: m })
+}
+handler.command = ['test']
+export default handler
