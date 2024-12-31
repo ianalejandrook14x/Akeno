5c0ffd99c4ba316a82476b59a0ca86bf4f4b06bd
Oscar66266 <oscarabrahamcabreramarin098@gmail.com>
2024-12-31 11:11:28 -0500

    Update descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -1,22 +1,19 @@
 import fetch from 'node-fetch'
 
-const handler = async (m, { conn, args, text, usedPrefix, command }) => {
-  if (!text) return conn.reply(m.chat, '✦ *Ingrese el enlace de una canción de Apple Music.*', m)
+let handler = async (m, { conn, usedPrefix, command, args }) => {
+  if (!args[0]) return m.reply('✦ *Ingresa un enlace de Apple Music.*')
   await m.react('🕓')
 
   try {
-    const apiUrl = `https://deliriussapi-oficial.vercel.app/download/applemusicdl?url=${encodeURIComponent(text)}`
-    const response = await fetch(apiUrl)
-    const data = await response.json()
+    let api = await fetch(`https://deliriussapi-oficial.vercel.app/download/applemusicdl?url=${args[0]}`)
+    let json = await api.json()
+    let { data } = json
+    let { type, name, image, artists, duration, download } = data
 
-    if (!data.status || !data.data) throw new Error('No se pudo obtener la información de la canción.')
+    let JT = `✦ *Título*\n» ${name}\n\n✦ *Artista*\n» ${artists}\n\n✦ *Tipo*\n» ${type}\n\n✦ *Duración*\n» ${duration}`
 
-    const { name, image, artists, duration, download } = data.data
-
-    const texto1 = `✦ *Título*\n» ${name}\n\n✦ *Artista*\n» ${artists}\n\n✦ *Duración*\n» ${duration}`.trim()
-    await conn.sendFile(m.chat, image, 'thumbnail.jpg', texto1, m)
-
-    await conn.sendMessage(m.chat, { document: { url: download }, mimetype: 'audio/mpeg', fileName: `${name}.mp3` }, { quoted: m })
+    await conn.sendFile(m.chat, image, 'thumbnail.jpg', JT, m)
+    await conn.sendFile(m.chat, download, `${name}.mp3`, null, m)
     await m.react('✅')
   } catch (error) {
     console.error(error)
