2f9d2928b871232a014a00af284889e019cdefed
Oscar66266 <oscarabrahamcabreramarin098@gmail.com>
2025-01-03 13:07:46 -0500

    Update descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -1,33 +1,29 @@
-
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args }) => {
-  if (!args[0]) return conn.reply(m.chat, '*\`Ingresa el nombre de lo que quieres buscar\`*', m);
+  if (!args[0]) {
+    return conn.reply(m.chat, estilo, m);
+  }
 
   await m.react('');
   try {
-    
     let query = args.join(" ");
     let searchApiResponse = await fetch(`https://restapi.apibotwa.biz.id/api/search-yts?message=${encodeURIComponent(query)}`);
     let searchResults = await searchApiResponse.json();
 
-    
     if (!searchResults.status || !searchResults.data || !searchResults.data.response || !searchResults.data.response.video || !searchResults.data.response.video.length) {
       return conn.reply(m.chat, '*\`No se encontraron resultados para tu búsqueda.\`*', m).then(_ => m.react('✖️'));
     }
 
-    
     let video = searchResults.data.response.video[0];
     let img = await (await fetch(video.thumbnail)).buffer();
 
-    
     let txt = `*\`Y O U T U B E - P L A Y\`*\n\n`;
     txt += `• *\`Título:\`* ${video.title}\n`;
     txt += `• *\`Duración:\`* ${parseDuration(video.duration)}\n`;
     txt += `• *\`Canal:\`* ${video.authorName || 'Desconocido'}\n`;
     txt += `• *\`Url:\`* ${video.url}\n\n`;
 
-    
     await conn.sendMessage(m.chat, {
       image: img,
       caption: txt,
@@ -68,3 +64,23 @@ function parseDuration(duration) {
   let parts = duration.split(':').reverse();
   return parts.reduce((total, part, index) => total + parseInt(part) * Math.pow(60, index), 0);
 }
+
+
+estilo = {
+  key: {
+    fromMe: false,
+    participant: `0@s.whatsapp.net`,
+    ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {})
+  },
+  message: {
+    orderMessage: {
+      itemCount: -999999,
+      status: 1,
+      surface: 1,
+      message: 'Ingresa el nombre de lo que quieres buscar',
+      orderTitle: 'Bang',
+      thumbnail: 'https://qu.ax/GSMZV.jpg', 
+      sellerJid: '0@s.whatsapp.net'
+    }
+  }
+};
