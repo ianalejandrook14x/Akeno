f9498d6e0f15832701910d2825d79363f4e16ab3
Oscar66266 <oscarabrahamcabreramarin098@gmail.com>
2025-01-03 12:54:37 -0500

    Update descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -1,54 +1,33 @@
+
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, args }) => {
-  if (!args[0]) {
-    // Respuesta combinada: texto + quoted de orden
-    await conn.sendMessage(m.chat, {
-      text: '*\`Ingresa el nombre de lo que quieres buscar\`*',
-      quoted: {
-        key: {
-          fromMe: false,
-          participant: `0@s.whatsapp.net`,
-          remoteJid: 'status@broadcast'
-        },
-        message: {
-          orderMessage: {
-            itemCount: 1, // Cantidad de ítems
-            status: 1, // Estado de la orden
-            surface: 1, // Superficie (plataforma)
-            message: '*\`Ingresa el nombre de lo que quieres buscar\`*', // Mensaje principal
-            orderTitle: 'Búsqueda de YouTube', // Título de la orden
-            //thumbnail: 'https://qu.ax/GSMZV.jpg', // Imagen de la orden
-            sellerJid: '0@s.whatsapp.net' // Identificador del vendedor
-          }
-        }
-      }
-    }, { quoted: m });
-
-    return;
-  }
+  if (!args[0]) return conn.reply(m.chat, '*\`Ingresa el nombre de lo que quieres buscar\`*', m);
 
   await m.react('');
   try {
+    
     let query = args.join(" ");
     let searchApiResponse = await fetch(`https://restapi.apibotwa.biz.id/api/search-yts?message=${encodeURIComponent(query)}`);
     let searchResults = await searchApiResponse.json();
 
+    
     if (!searchResults.status || !searchResults.data || !searchResults.data.response || !searchResults.data.response.video || !searchResults.data.response.video.length) {
       return conn.reply(m.chat, '*\`No se encontraron resultados para tu búsqueda.\`*', m).then(_ => m.react('✖️'));
     }
 
+    
     let video = searchResults.data.response.video[0];
     let img = await (await fetch(video.thumbnail)).buffer();
 
-    // Texto del mensaje
+    
     let txt = `*\`Y O U T U B E - P L A Y\`*\n\n`;
     txt += `• *\`Título:\`* ${video.title}\n`;
     txt += `• *\`Duración:\`* ${parseDuration(video.duration)}\n`;
     txt += `• *\`Canal:\`* ${video.authorName || 'Desconocido'}\n`;
     txt += `• *\`Url:\`* ${video.url}\n\n`;
 
-    // Enviar el mensaje con los resultados de búsqueda
+    
     await conn.sendMessage(m.chat, {
       image: img,
       caption: txt,
