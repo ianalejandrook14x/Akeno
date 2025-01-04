1e35a1defb806e44e2e35c0ed033ac6985675bad
ゞ 𝐆𝐚𝐛𝐫𝐢𝐞𝐥 𝐒𝐤 ぁ <148560941+glytglobal@users.noreply.github.com>
2025-01-04 11:48:54 -0300

    `Akeno Himajima - Bot`

--- plugins/rpg•perfil.js
+++ plugins/rpg•perfil.js
@@ -5,13 +5,13 @@ let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? c
 let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60')
 let { premium, level, yenes, exp, lastclaim, registered, regTime, age, role } = global.db.data.users[m.sender]
 let username = conn.getName(who)
-let noprem = `
+let perfil = `
 ✦ *PERFIL DEL USUARIO*
 ✦ *Nombre:* ${username}
-✦ *${currency}:* ${money || 'Sin Información'}
+✦ *${currency}:* ${m.yenes || 'Sin Información'}
 ✦ *Experiencia:* ${exp || 'Sin Información'}
 ✦ *Premium*: *${prems ? 'Si' : 'No'}*`.trim()
-conn.sendFile(m.chat, pp, 'perfil.jpg', `${premium ? prem.trim() : noprem.trim()}`, m, rcanal, { mentions: [who] })
+conn.sendFile(m.chat, pp, 'perfil.jpg', perfil, m, rcanal, { mentions: [who] })
 }
 handler.help = ['profile']
 handler.tags = ['rpg']
