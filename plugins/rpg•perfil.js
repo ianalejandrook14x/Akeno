9289d85727bb5e0c96ef0343794ad36fc0e7cf68
ゞ 𝐆𝐚𝐛𝐫𝐢𝐞𝐥 𝐒𝐤 ぁ <148560941+glytglobal@users.noreply.github.com>
2025-01-04 12:36:21 -0300

    `Akeno Himajima - Bot`

--- plugins/rpg•perfil.js
+++ plugins/rpg•perfil.js
@@ -1,14 +1,16 @@
 import PhoneNumber from 'awesome-phonenumber'
 import fetch from 'node-fetch'
-var handler = async (m, { conn, yenes }) => {
+var handler = async (m, { conn, isPrems }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60')
 let username = conn.getName(who)
-let currency = `Moneda`
+let { currency, yenes, exp } = global.db.data.users[who]
 let perfil = `
-✦ *PERFIL DEL USUARIO*
+✎ \`\`\`PERFIL DEL USUARIO\`\`\`
 ✦ *Nombre:* ${username}
-✦ *${currency}:* ${yenes || 'Sin Información'}`.trim()
+✦ *${currency}:* ${yenes || 'Sin Información'}
+✦ *Experiencia:* ${exp || 'Sin Información'}
+✦ *Premium*: *${isPrems ? 'Si' : 'No'}*`.trim()
 conn.sendFile(m.chat, pp, 'perfil.jpg', perfil, m, rcanal, { mentions: [who] })
 }
 handler.help = ['profile']
