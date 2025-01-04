1c35cf7a15d3a64480d4085d65f1f746f61835cd
ゞ 𝐆𝐚𝐛𝐫𝐢𝐞𝐥 𝐒𝐤 ぁ <148560941+glytglobal@users.noreply.github.com>
2025-01-04 12:15:31 -0300

    `Akeno Himajima - Bot`

--- plugins/rpg•perfil.js
+++ plugins/rpg•perfil.js
@@ -1,9 +1,8 @@
 import PhoneNumber from 'awesome-phonenumber'
 import fetch from 'node-fetch'
-var handler = async (m, { conn }) => {
+var handler = async (m, { conn, yenes }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60')
-let { premium, level, yenes, exp, lastclaim, registered, regTime, age, role } = global.db.data.users[m.sender]
 let username = conn.getName(who)
 let perfil = `
 ✦ *PERFIL DEL USUARIO*
