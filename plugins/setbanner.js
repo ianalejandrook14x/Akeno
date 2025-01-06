54b310df1a3efa8dac1ed4927cf02c9ef17d1f24
Ian.xyz <taiwam34@gmail.com>
2025-01-06 13:16:47 -0300

    Update setbanner.js

--- plugins/setbanner.js
+++ plugins/setbanner.js
@@ -6,64 +6,45 @@ import { FormData, Blob } from "formdata-node";
 import { fileTypeFromBuffer } from "file-type";
 
 let handler = async (m, { conn, isRowner }) => {
-
   if (!m.quoted || !/image|video/.test(m.quoted.mimetype)) return m.reply('✦ *Responda a una imagen con el comando para cambiar el banner*.');
 
   try {
-
     const media = await m.quoted.download();
     let link = await catbox(media);
-    
+
     if (!isImageValid(media)) {
       return m.reply('✧ El archivo enviado no es una imagen válida.');
     }
 
-    global.banner = `${link}`;  
-m.reply('*✦ El banner fue actualizado*')
+    const userId = m.sender;
+    if (!global.subbots) global.subbots = {};
 
+    if (!global.subbots[userId]) {
+      return m.reply('*Este comando solo puede ser usado por el dueño del PreBot*');
+    }
+
+    global.subbots[userId].banner = `${link}`;
+    m.reply('*✦ El banner de tu subbot fue actualizado*');
   } catch (error) {
     console.error(error);
     m.reply('*✧ Hubo un error al intentar cambiar el banner.*');
   }
 };
 
-
 const isImageValid = (buffer) => {
   const magicBytes = buffer.slice(0, 4).toString('hex');
-
-
-  if (magicBytes === 'ffd8ffe0' || magicBytes === 'ffd8ffe1' || magicBytes === 'ffd8ffe2') {
+  if (magicBytes === 'ffd8ffe0' || magicBytes === 'ffd8ffe1' || magicBytes === 'ffd8ffe2' || magicBytes === '89504e47' || magicBytes === '47494638') {
     return true;
   }
-
-
-  if (magicBytes === '89504e47') {
-    return true;
-  }
-
-
-  if (magicBytes === '47494638') {
-    return true;
-  }
-
   return false; 
 };
 
 handler.help = ['setbanner'];
-handler.tags = ['tools'];
+handler.tags = ['customization'];
 handler.command = ['setbanner'];
 handler.mods = true;
 
 export default handler;
-
-function formatBytes(bytes) {
-  if (bytes === 0) {
-    return "0 B";
-  }
-  const sizes = ["B", "KB", "MB", "GB", "TB"];
-  const i = Math.floor(Math.log(bytes) / Math.log(1024));
-  return `${(bytes / 1024 ** i).toFixed(2)} ${sizes[i]}`;
-}
 
 async function catbox(content) {
   const { ext, mime } = (await fileTypeFromBuffer(content)) || {};
