beb4fe3a6a834ec883b650c149d7a754aef96226
Ian.xyz <taiwam34@gmail.com>
2025-01-06 15:43:39 -0300

    Update Main•allmenu.js

--- plugins/Main•allmenu.js
+++ plugins/Main•allmenu.js
@@ -1,144 +1,144 @@
 let handler = async (m, { conn }) => {
   let txt = `*Hola soy ${botname}* ✧
 
-> *INFO*
+ *\`INFO\`*
 
-*perfil*
-*menu*
-*grupos*
-*blocklist*
+❀ *perfil*
+❀ *menu*
+❀ *grupos*
+❀ *blocklist*
 
-> *BUSQUEDAS*
+ *\`BUSQUEDAS\`*
 
-*google <búsqueda>*
-*tiktoksearch <txt>*
-*ytsearch*
-*imagen <txt>*
-*play* <musica>
-*ytmp4* <nombre>
-*ytmp3* <nombre>
-
-
-> *JUEGOS*
-
-*abrazar <@tag>*
-*acertijo*
-*sonrojarse <@tag>*
-*consejo*
-*enamorada <@tag>*
-*meme*
-*acariciar <@tag>*
-*personalidad*
-*piropo*
-*pokedex <pokemón>*
-*pregunta*
-*dormir <@tag>*
-*triste <@tag>*
-*top <txt>*
-*zodiac <2010 03 15*
-
-> *SUB BOTS*
-
-*code* 
-*serbot*
-
-> *RPG*
-
-*bal*
-*crimen*
-*daily*
-*claim*
-*depositar*
-*lb*
-*retirar*
-*rob2*
-*rob*
-*trabajar*
-*buy*
-*buy all*
-
-> *STICKERS*
-
-*qc*
-*stiker <img>*
-*sticker <url>*
-*take <nombre/autor>*
-
-> *ANIMES*
-
-*rule34 <tag>*
-*waifu*
-
-> *GRUPOS*
-
-*link*
-*grupo open / close*
-*delete*
-*demote*
-*promote*
-*encuesta <txt / txt>*
-*hidetag*
-*infogrupo*
-*kick*
-*listadv*
-*tagall <txt>*
-*invocar <txt>*
-
-> *ON/OFF*
-
-*enable*
-*disable*
-
-> *DESCARGAS*
-
-*facebook - fb*
-*imagen <txt>*
-*instagram - ig*
-*tiktok*
-*ytmp4*
-*ytmp3*
+❀ *google <búsqueda>*
+❀ *tiktoksearch <txt>*
+❀ *ytsearch*
+❀ *imagen <txt>*
+❀ *play* <musica>
+❀ *ytmp4* <nombre>
+❀ *ytmp3* <nombre>
 
 
-> *HERRAMIENTAS*
+ *\`JUEGOS\`*
 
-*toanime*
-*remini*
-*hd*
-*enhance*
-*ssweb*
-*ss*
-*trad*
+❀ *abrazar <@tag>*
+❀ *acertijo*
+❀ *sonrojarse <@tag>*
+❀ *consejo*
+❀ *enamorada <@tag>*
+❀ *meme*
+❀ *acariciar <@tag>*
+❀ *personalidad*
+❀ *piropo*
+❀ *pokedex <pokemón>*
+❀ *pregunta*
+❀ *dormir <@tag>*
+❀ *triste <@tag>*
+❀ *top <txt>*
+❀ *zodiac <2010 03 15*
 
-> *AUDIOS*
+ *\`JADI BOTS\`*
 
-*bass <vn>*
-*blown <vn>*
-*deep <vn>*
-*earrape <vn>*
-*fast <vn>*
-*fat <vn>*
-*nightcore <vn>*
-*reverse <vn>*
-*robot <vn>*
-*slow <vn>*
-*smooth <vn>*
-*tupai <vn>*
+❀ *code* 
+❀ *serbot*
 
-> *AI*
+ *\`RPG\`*
 
-*remini*
-*hd*
-*enhance*
+❀ *bal*
+❀ *crimen*
+❀ *daily*
+❀ *claim*
+❀ *depositar*
+❀ *lb*
+❀ *retirar*
+❀ *rob2*
+❀ *rob*
+❀ *trabajar*
+❀ *buy*
+❀ *buy all*
 
-> *CONVERTIDORES*
+ *\`STICKERS\`*
 
-*togifaud*
-*toimg*
+❀ *qc*
+❀ *stiker <img>*
+❀ *sticker <url>*
+❀ *take <nombre/autor>*
 
-> *ADMIN*
+ *\`ANIMES\`*
 
-*addprem2 <@tag> <days>*
-*addyen2 <@tag>*`.trim();
+❀ *rule34 <tag>*
+❀ *waifu*
+
+ *\`GRUPOS\`*
+
+❀ *link*
+❀ *grupo open / close*
+❀ *delete*
+❀ *demote*
+❀ *promote*
+❀ *encuesta <txt / txt>*
+❀ *hidetag*
+❀ *infogrupo*
+❀ *kick*
+❀ *listadv*
+❀ *tagall <txt>*
+❀ *invocar <txt>*
+
+ *\`ON/OFF\`*
+
+❀ *enable*
+❀ *disable*
+
+ *\`DESCARGAS\`*
+
+❀ *facebook - fb*
+❀ *imagen <txt>*
+❀ *instagram - ig*
+❀ *tiktok*
+❀ *ytmp4*
+❀ *ytmp3*
+
+ *\`HERRAMIENTAS\`*
+
+❀ *toanime*
+❀ *remini*
+❀ *hd*
+❀ *enhance*
+❀ *ssweb*
+❀ *ss*
+❀ *trad*
+
+ *\`AUDIOS\`*
+
+❀ *bass <vn>*
+❀ *blown <vn>*
+❀ *deep <vn>*
+❀ *earrape <vn>*
+❀ *fast <vn>*
+❀ *fat <vn>*
+❀ *nightcore <vn>*
+❀ *reverse <vn>*
+❀ *robot <vn>*
+❀ *slow <vn>*
+❀ *smooth <vn>*
+❀ *tupai <vn>*
+
+ *\`AI\`*
+
+❀ *remini*
+❀ *hd*
+❀ *enhance*
+
+ *\`CONVERTIDORES\`*
+
+❀ *togifaud*
+❀ *toimg*
+❀ *toaudio*
+
+ *\`ADMIN\`*
+
+❀ *addprem2 <@tag> <days>*
+❀ *addyen2 <@tag>*`.trim();
 
 m.react('✅');
 let perfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://qu.ax/QGAVS.jpg');
