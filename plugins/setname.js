165535908c6e3c561156e3fe8273662ace689991
Ian.xyz <taiwam34@gmail.com>
2025-01-08 20:41:20 -0300

    Update setname.js

--- plugins/setname.js
+++ plugins/setname.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, isRowner }) => {
   const newName = m.text.trim().split(' ').slice(1).join(' ');
 
-  if (!newName) {
+  if (!newJadiname) {
     return m.reply('*Proporciona un nuevo nombre para el bot ✦*');
   }
 
@@ -12,8 +12,8 @@ let handler = async (m, { conn, isRowner }) => {
     return m.reply('*Este comando solo puede ser usado por el dueño del PreBot*');
   }
 
-  global.subbots[userId].name = newName;
-  m.reply(`*El nombre de tu subbot ha sido actualizado a: ${newName} ✦*`);
+  global.subbots[userId].name = newJadiname;
+  m.reply(`*El nombre de tu subbot ha sido actualizado a: ${newJadiname} ✦*`);
 };
 
 handler.help = ['setname'];
