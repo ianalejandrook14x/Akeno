b46b420e22cc8f8c88f70f3e0fb1b920bafad9ec
Ian.xyz <taiwam34@gmail.com>
2025-01-08 23:37:13 -0300

    Update setcurrency.js

--- plugins/setcurrency.js
+++ plugins/setcurrency.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, isRowner }) => {
     const newCurrency = m.text.trim().split(' ').slice(1).join(' ');
 
-    if (!newJadicurrency) {
+    if (!newCurrency) {
         return m.reply('*Proporciona una nueva moneda (por ejemplo, USD, EUR)*');
     }
 
@@ -12,8 +12,8 @@ let handler = async (m, { conn, isRowner }) => {
       return m.reply('*Este comando solo puede ser usado por el dueño del PreBot*');
     }
 
-    global.subbots[userId].jadicurrency = newJadicurrency;
-    m.reply(`*La moneda de tu subbot ha sido actualizada a: ${newJadicurrency}*`);
+    global.subbots[userId].currency = newCurrency;
+    m.reply(`*La moneda de tu subbot ha sido actualizada a: ${newCurrency}*`);
 };
 
 handler.help = ['setcurrency'];
