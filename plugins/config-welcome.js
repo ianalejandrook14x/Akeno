5a6042cdca2b364706010259adacdaff5f9139e3
Ian.xyz <taiwam34@gmail.com>
2025-01-11 16:45:15 -0300

    Update config-welcome.js

--- plugins/config-welcome.js
+++ plugins/config-welcome.js
@@ -3,8 +3,6 @@ import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
-    let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => adios)
-  let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
 
  if (chat.welcome && m.messageStubType == 27) {
