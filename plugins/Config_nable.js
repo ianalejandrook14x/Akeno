f8edf0c4871d5cb67209ad8c96f76218bb2eb71d
Ian.xyz <taiwam34@gmail.com>
2025-01-16 20:37:32 -0300

    Update Config_nable.js

--- plugins/Config_nable.js
+++ plugins/Config_nable.js
@@ -259,43 +259,43 @@ ${usedPrefix + command} subbots
 *✧ Funciones de grupos*
 
 ${usedPrefix + command} reaccion 
-➤ *Esta función permita para que el bot pueda reaccionar a mensajes con cualquier emoji*
+> ➤ *Esta función permita para que el bot pueda reaccionar a mensajes con cualquier emoji*
 
 ${usedPrefix + command} antitraba
-➤ *Esta función permite eliminar a usuarios que tengan textos que contengan muchos caracteres*
+> ➤ *Esta función permite eliminar a usuarios que tengan textos que contengan muchos caracteres*
 
 ${usedPrefix + command} modoadmin 
-➤ *Esta función permite que solo administradores del grupo puedan utilizar el bot*
+> ➤ *Esta función permite que solo administradores del grupo puedan utilizar el bot*
 
 ${usedPrefix + command} antiarabes
-➤ *Esta función cuando esta activa elimina a los usuarios arabes cuando mandan un mensaje automaticamente*
+> ➤ *Esta función cuando esta activa elimina a los usuarios arabes cuando mandan un mensaje automaticamente*
 
 ${usedPrefix + command} simi
-➤ *Esta función te permite hablar con Simi una IA 
+> ➤ *Esta función te permite hablar con Simi una IA*
 
 ${usedPrefix + command} autoread
-➤ *Solo esta disponible para Jadibot o bot general trata de leer mensajes de grupos o chats cuando ingresan algun comando*
+> ➤ *Solo esta disponible para Jadibot o bot general trata de leer mensajes de grupos o chats cuando ingresan algun comando*
 
 ${usedPrefix + command} antiver
-➤ *Esta función permite que un usuario que mando una imagen o video para 1 sola vez, el bot lo envia sin restricción*
+> ➤ *Esta función permite que un usuario que mando una imagen o video para 1 sola vez, el bot lo envia sin restricción*
 
 ${usedPrefix + command} detect 
-➤ *Esta función solo esta disponible para grupos cuando alguien realiza algun cambio en el grupo el bot lo detecta*
+> ➤ *Esta función solo esta disponible para grupos cuando alguien realiza algun cambio en el grupo el bot lo detecta*
 
 ${usedPrefix + command} delete
-➤ *Esta función permite enviar el mensaje, imagen o video que algun usuario haya eliminado*
+> ➤ *Esta función permite enviar el mensaje, imagen o video que algun usuario haya eliminado*
 
 ${usedPrefix + command} nsfw
-➤ *Esta función permite activar los comandos +18*
+> ➤ *Esta función permite activar los comandos +18*
 
 ${usedPrefix + command} welcome
-➤ *Esta función permite a los nuevo usuarios que sean recibidos por un mensaje de bienvenida*
+> ➤ *Esta función permite a los nuevo usuarios que sean recibidos por un mensaje de bienvenida*
 
 ${usedPrefix + command} antibot
-➤ *Esta función permite eliminar a todos los bots que se encuentren activo en el grupo*
+> ➤ *Esta función permite eliminar a todos los bots que se encuentren activo en el grupo*
 
 ${usedPrefix + command} antilink
-➤ *Esta función elimina a los usuarios que hayan enviado algun link de algn grupo o canal`, m, )
+> ➤ *Esta función elimina a los usuarios que hayan enviado algun link de algn grupo o canal`, m, )
       throw false
   }
   conn.reply(m.chat, `✧ La función *${type}* se *${isEnable ? 'activó' : 'desactivó'}* ${isAll ? 'para este Bot' : isUser ? '' : 'para este chat'}`, m, )
