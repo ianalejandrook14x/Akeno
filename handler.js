5e7ee201f7440cdc6b69da49a07160052f248459
Ian.xyz <taiwam34@gmail.com>
2025-01-23 22:21:01 -0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -424,7 +424,7 @@ await plugin.after.call(this, m, extra)
 console.error(e)
 }}
 if (m.coin)
-conn.reply(m.chat, `Utilizaste ${+m.coin} ${global.moneda}`, m)
+conn.reply(m.chat, `Utilizaste ${+m.coin} ${global.currency}`, m)
 }
 break
 }}
