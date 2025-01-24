f24d1ff2f615e24b7f7ef118efd9727768075b89
Ian.xyz <taiwam34@gmail.com>
2025-01-23 23:26:41 -0300

    Update setbanner.js

--- plugins/setbanner.js
+++ plugins/setbanner.js
@@ -6,45 +6,64 @@ import { FormData, Blob } from "formdata-node";
 import { fileTypeFromBuffer } from "file-type";
 
 let handler = async (m, { conn, isRowner }) => {
-  if (!m.quoted || !/image|video/.test(m.quoted.mimetype)) return m.reply('✦ *Responda a una imagen con el comando para cambiar el banner*.');
+
+  if (!m.quoted || !/image/.test(m.quoted.mimetype)) return m.reply('✦ *Responda a una imagen con el comando para cambiar el banner*.');
 
   try {
+
     const media = await m.quoted.download();
     let link = await catbox(media);
-
+    
     if (!isImageValid(media)) {
       return m.reply('✧ El archivo enviado no es una imagen válida.');
     }
 
-    const userId = m.sender;
-    if (!global.subbots) global.subbots = {};
+    global.banner = `${link}`;  
+m.reply('*✦ El banner fue actualizado*')
 
-    if (!global.subbots[userId]) {
-      return m.reply('*Este comando solo puede ser usado por el dueño del PreBot*');
-    }
-
-    global.subbots[userId].banner = `${link}`;
-    m.reply('*✦ El banner de tu subbot fue actualizado*');
   } catch (error) {
     console.error(error);
     m.reply('*✧ Hubo un error al intentar cambiar el banner.*');
   }
 };
 
+
 const isImageValid = (buffer) => {
   const magicBytes = buffer.slice(0, 4).toString('hex');
-  if (magicBytes === 'ffd8ffe0' || magicBytes === 'ffd8ffe1' || magicBytes === 'ffd8ffe2' || magicBytes === '89504e47' || magicBytes === '47494638') {
+
+
+  if (magicBytes === 'ffd8ffe0' || magicBytes === 'ffd8ffe1' || magicBytes === 'ffd8ffe2') {
     return true;
   }
+
+
+  if (magicBytes === '89504e47') {
+    return true;
+  }
+
+
+  if (magicBytes === '47494638') {
+    return true;
+  }
+
   return false; 
 };
 
 handler.help = ['setbanner'];
-handler.tags = ['customization'];
+handler.tags = ['tools'];
 handler.command = ['setbanner'];
-handler.mods = false;
+handler.mods = true;
 
 export default handler;
+
+function formatBytes(bytes) {
+  if (bytes === 0) {
+    return "0 B";
+  }
+  const sizes = ["B", "KB", "MB", "GB", "TB"];
+  const i = Math.floor(Math.log(bytes) / Math.log(1024));
+  return `${(bytes / 1024 ** i).toFixed(2)} ${sizes[i]}`;
+}
 
 async function catbox(content) {
   const { ext, mime } = (await fileTypeFromBuffer(content)) || {};
