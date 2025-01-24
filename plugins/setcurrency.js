338e62dd128f583cf7378b5d68182483832d6e19
Ian.xyz <taiwam34@gmail.com>
2025-01-23 23:27:05 -0300

    Update setcurrency.js

--- plugins/setcurrency.js
+++ plugins/setcurrency.js
@@ -1,24 +1,18 @@
 let handler = async (m, { conn, isRowner }) => {
-    const newCurrency = m.text.trim().split(' ').slice(1).join(' ');
+    const newCurrency = m.text.trim().split(' ').slice(1).join(' '); 
 
     if (!newCurrency) {
         return m.reply('*Proporciona una nueva moneda (por ejemplo, USD, EUR)*');
     }
 
-    const userId = m.sender;
-    if (!global.subbots) global.subbots = {};
+    global.currency = newCurrency;
 
-    if (!global.subbots[userId]) {
-      return m.reply('*Este comando solo puede ser usado por el dueño del PreBot*');
-    }
-
-    global.subbots[userId].currency = newCurrency;
-    m.reply(`*La moneda de tu subbot ha sido actualizada a: ${newCurrency}*`);
+    m.reply(`*La moneda ha sido actualizada a: ${newCurrency}*`);
 };
 
-handler.help = ['setcurrency'];
-handler.tags = ['customization'];
-handler.command = ['setcurrency'];
-handler.mods = false;
+handler.help = ['setcurrency']; 
+handler.tags = ['banner'];
+handler.command = ['setcurrency']; 
+handler.mods = true
 
 export default handler;
