9ce7ab888d3e8939ce090358e582bee6d24ebc44
Oscar66266 <oscarabrahamcabreramarin098@gmail.com>
2025-01-26 11:42:56 -0500

    Update Prueba.js

--- plugins/Prueba.js
+++ plugins/Prueba.js
@@ -1,6 +1,5 @@
 import fetch from 'node-fetch';
 import yts from 'yt-search';
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
 
 let limit = 94;
 
@@ -17,14 +16,20 @@ let handler = async (m, { conn: star, args, usedPrefix, command }) => {
 
   try {
     let v = args[0];
-    let yt = await youtubedl(v).catch(async () => await youtubedlv2(v));
-    let { title, thumbnail, timestamp, views, ago } = yt;
+    let ytres = await yts(v);
+    let video = ytres.videos[0];
 
-    let api = await fetch(`https://api.vreden.web.id/api/ytmp3?url=${v}`);
+    if (!video) {
+      return star.reply(m.chat, '✦ *Video no encontrado.*', m).then(_ => m.react('✖️'));
+    }
+
+    let { title, thumbnail, timestamp, views, ago } = video;
+
+    let api = await fetch(`https://api.vreden.web.id/api/ytplaymp3?query=${v}`);
     let json = await api.json();
-    let { result } = json;
-    let { download, size } = result;
+    let { download } = json.result;
 
+    let size = (await fetch(download.url, { method: 'HEAD' })).headers.get('content-length');
     size = (size / (1024 * 1024)).toFixed(2);
 
     if (size >= 700) {
@@ -36,14 +41,14 @@ let handler = async (m, { conn: star, args, usedPrefix, command }) => {
     txt += `✦ *Visitas:* » ${views}\n`;
     txt += `✦ *Subido:* » ${ago}\n`;
     txt += `✦ *Tamaño:* » ${size} MB\n\n`;
-   // txt += `> *- ↻ El audio se está enviando, espera un momento...*`;
+  //  txt += `> *- ↻ El audio se está enviando, espera un momento...*`;
 
     await star.sendFile(m.chat, thumbnail, 'thumbnail.jpg', txt, m);
 
     if (size >= limit) {
-      await star.sendMessage(m.chat, { document: { url: download }, mimetype: 'audio/mp4', fileName: `${title}.m4a` }, { quoted: m });
+      await star.sendMessage(m.chat, { document: { url: download.url }, mimetype: 'audio/mp4', fileName: `${title}.m4a` }, { quoted: m });
     } else {
-      await star.sendMessage(m.chat, { audio: { url: download }, mimetype: 'audio/mpeg', fileName: `${title}.mp3` }, { quoted: m });
+      await star.sendMessage(m.chat, { audio: { url: download.url }, mimetype: 'audio/mpeg', fileName: `${title}.mp3` }, { quoted: m });
     }
 
     await m.react('✅');
