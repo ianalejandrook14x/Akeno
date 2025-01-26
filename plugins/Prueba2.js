92b7ef9ea474c4c8ec47907820728dc83670198e
Oscar66266 <oscarabrahamcabreramarin098@gmail.com>
2025-01-26 12:08:07 -0500

    Update Prueba2.js

--- plugins/Prueba2.js
+++ plugins/Prueba2.js
@@ -1,20 +1,36 @@
+/* 
 
-import fetch from 'node-fetch';
+[ Canal Principal ] :
+https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
+
+[ Canal Rikka Takanashi Bot ] :
+https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
+
+[ Canal StarlightsTeam] :
+https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
+
+[ HasumiBot FreeCodes ] :
+https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
+*/
+
+// *[ ❀ YTMP4 ]*
+import fetch from 'node-fetch'
 
 let HS = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, `❀ Ingresa un link de youtube`, m)
-
+if (!text) {
+return m.reply("❀ Ingresa un link de youtube")
+}
+    
 try {
-let api = await fetch(`https://restapi.apibotwa.biz.id/api/ytmp4?url=${text}`)
+let api = await fetch(`https://api.giftedtech.my.id/api/download/dlmp4?apikey=gifted&url=${text}`)
 let json = await api.json()
-let title = json.data.metadata.title
-let dl_url = json.data.download.url
-await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${json.data.filename}.mp4`, mimetype: "video/mp4" }, { quoted: m })
+let { quality, title, download_url } = json.result
 
+await conn.sendMessage(m.chat, { video: { url: download_url }, caption: `${title}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: m })
 } catch (error) {
 console.error(error)
 }}
 
-HS.command = ['ytmp4']
+HS.command = /^(ytmp4)$/i
 
 export default HS
