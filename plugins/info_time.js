14d337d902d3bab11835daecab28966d1cc8a370
Ian.xyz <taiwam34@gmail.com>
2025-02-01 11:28:44 -0300

    Create info_time.js

---
+++ plugins/info_time.js
@@ -0,0 +1,18 @@
+const handler = async (m, { conn }) => {
+    const _muptime = process.uptime() * 1000;
+    const muptime = clockString(_muptime);
+    
+    const message = `✦ *Tiempo Activo:* ${muptime}`;
+    await conn.reply(m.chat, message.trim(), m);
+};
+
+handler.command = ['uptime', 'actividad'];
+
+export default handler;
+
+function clockString(ms) {
+    let h = Math.floor(ms / 3600000);
+    let m = Math.floor(ms / 60000) % 60;
+    let s = Math.floor(ms / 1000) % 60;
+    return [h, m, s].map(v => v.toString().padStart(2, '0')).join(':');
+}
