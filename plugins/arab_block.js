2aa7fd54e81f165cb7f913f20c884f6c8ccc097a
Ian.xyz <taiwam34@gmail.com>
2025-02-01 17:15:02 -0300

    Update arab_block.js

--- plugins/arab_block.js
+++ plugins/arab_block.js
@@ -1,15 +1,21 @@
-const arabPrefixes = ['+212', '+213', '+216', '+218', '+20', '+970', '+963', '+966', '+967', '+968', '+971', '+972', '+973', '+974', '+975', '+976', '+977']; 
-const handler = async (m, { conn }) => {
+const arabPrefixes = ['+212', '+213', '+216', '+218', '+20', '+970', '+963', '+966', '+967', '+968', '+971', '+972', '+973', '+974', '+975', '+976', '+977'];
+
+const handler = async (m, { conn, isOwner }) => {
     const sender = m.sender;
-    
+
+    if (isOwner) return;
+
     if (arabPrefixes.some(prefix => sender.startsWith(prefix))) {
         if (!m.isGroup) {
-            await conn.updateBlockStatus(sender, 'block');
+            await conn.updateBlockStatus(sender, 'block').catch(() => {});
         }
-        return;
+        return; 
     }
 };
 
-handler.before = handler;
+handler.before = async (m, args) => {
+    await handler(m, args);
+    return false; 
+};
 
 export default handler;
