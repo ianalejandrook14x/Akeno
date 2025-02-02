f68884ad87da39c05d3f4faab711403dbcc04c70
Ian.xyz <taiwam34@gmail.com>
2025-02-01 21:35:14 -0300

    Update owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -1,13 +1,26 @@
-let handler = async (m, { conn, text}) => {
-let who
-if (m.isGroup) who = m.mentionedJid[0]
-else who = m.chat
-let users = global.db.data.users
-users[who].banned = true
-conn.reply(m.chat, `✦ *El usuario @${who.split('@')[0]} Fue baneado*`, fkontak, { mentions: [who]})
-}
-handler.help = ['banuser <@tag> <razón>']
-handler.command = ['banuser']
-handler.tags = ['owner']
-handler.rowner = true
-export default handler
+let handler = async (m, { conn, text }) => {
+    let who;
+    if (m.isGroup) who = m.mentionedJid[0];
+    else who = m.chat;
+
+    let users = global.db.data.users;
+    users[who].banned = true;
+
+    conn.reply(m.chat, `✦ *El usuario @${who.split('@')[0]} Fue baneado*`, fkontak, { mentions: [who] });
+};
+
+export const before = async (m, { conn }) => {
+    const userId = m.sender;
+    let users = global.db.data.users;
+
+    if (users[userId] && users[userId].banned) {
+        return false;
+    }
+
+    return true; 
+};
+
+handler.command = ['banuser'];
+handler.rowner = true;
+
+export default handler;
