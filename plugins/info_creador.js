db9a08717cde36bbc3a654c0aa652eee1356d1d6
Ian.xyz <taiwam34@gmail.com>
2025-02-01 22:05:31 -0300

    Update info_creador.js

--- plugins/info_creador.js
+++ plugins/info_creador.js
@@ -1,9 +1,15 @@
 function handler(m) {
-    let numc = '+5493876639332';
+    let numc = '5493876639332';
+
     let name = conn.getName(`${numc}@s.whatsapp.net`);
-    let ownerN = `${numc}`;
-  
-    conn.sendContact(m.chat, [[`${ownerN}@s.whatsapp.net`, `${name}`]], m);
+
+    conn.getProfilePicture(`${numc}@s.whatsapp.net`).then(pic => {
+        conn.sendContact(m.chat, [[`${numc}@s.whatsapp.net`, `${name}`]], m, {
+            thumbnail: pic,
+        });
+    }).catch(() => {
+        conn.sendContact(m.chat, [[`${numc}@s.whatsapp.net`, `${name}`]], m);
+    });
 }
 
 handler.command = ['owner', 'creator', 'creador', 'dueño'];
