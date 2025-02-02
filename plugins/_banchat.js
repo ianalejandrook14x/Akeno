017fe530613c9d720e4d98915dce5befef531f7a
Ian.xyz <taiwam34@gmail.com>
2025-02-01 21:52:45 -0300

    Create _banchat.js

---
+++ plugins/_banchat.js
@@ -0,0 +1,15 @@
+let handler = async (m, { conn }) => {
+if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '*Este chat no se encuentra registrado*', m, fake)
+let chat = global.db.data.chats[m.chat]
+if (!chat.isBanned) return conn.reply(m.chat, '*El bot fue desactivado en este chat*', m, fake)
+chat.isBanned = false
+await conn.reply(m.chat, '*El bot ha sido reactivado*', m, fake)
+}
+handler.help = ['unbanchat'];
+handler.tags = ['grupo'];
+handler.command = ['unbanchat','desbanearchat','desbanchat']
+handler.admin = true 
+handler.botAdmin = true
+handler.group = true
+
+export default handler
