86fa26e2d91a764b527eb88780284eb4548311d8
Ian.xyz <taiwam34@gmail.com>
2025-02-01 22:13:05 -0300

    Update info_creador.js

--- plugins/info_creador.js
+++ plugins/info_creador.js
@@ -1,16 +1,57 @@
-function handler(m) {
-    let numcr = '5493876639332';
-    let name = conn.getName(`${numcr}@s.whatsapp.net`);
+import PhoneNumber from 'awesome-phonenumber'
 
-    conn.profilePictureUrl(`${numcr}@s.whatsapp.net`, 'image').then(pic => {
-        conn.sendContact(m.chat, [[`${numcr}@s.whatsapp.net`, `${name}`]], m, {
-            thumbnail: pic,
-        });
-    }).catch(() => {
-        conn.sendContact(m.chat, [[`${numcr}@s.whatsapp.net`, `${name}`]], m);
-    });
-}
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+let biografia = await conn.fetchStatus('5493876639332' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
+let biografiaBot = await conn.fetchStatus(`${conn.user.jid.split('@')[0]}` +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
+let bio = biografia.status?.toString() || 'Sin Biografía'
+let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
+let name = await conn.getName(who)
 
-handler.command = ['owner', 'creator', 'creador', 'dueño'];
+  await sendContactArray(conn, m.chat, [
+     [`${nomorown}`, `Creador`, `Ian.xyz`, dev, md, `🇦🇷 Argentina`, `${global.botname}`, bio]
+], m)
+  } 
 
-export default handler;
+handler.command = ['owner','creador']
+
+export default handler
+
+async function sendContactArray(conn, jid, data, quoted, options) {
+        if (!Array.isArray(data[0]) && typeof data[0] === 'string') data = [data]
+                let contacts = []
+        for (let [number, name, isi, isi1, isi2, isi3, isi4, isi5] of data) {
+            number = number.replace(/[^0-9]/g, '')
+            let njid = number + '@s.whatsapp.net'
+            let biz = await conn.getBusinessProfile(njid).catch(_ => null) || {}
+            let vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:Sy;Bot;;;
+FN:${name.replace(/\n/g, '\\n')}
+item.ORG:${isi}
+item1.TEL;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}
+item1.X-ABLabel:${isi1}
+item2.EMAIL;type=INTERNET:${isi2}
+item2.X-ABLabel:📧 Email
+item3.ADR:;;${isi3};;;;
+item3.X-ABADR:ac
+item3.X-ABLabel:🏷 Region
+item4.URL:${isi4}
+item4.X-ABLabel:Website
+item5.X-ABLabel:${isi5}
+END:VCARD`.trim()
+            contacts.push({ vcard, displayName: name })
+        }
+        return await conn.sendMessage(jid, {
+            contacts: {
+                displayName: (contacts.length > 1 ? `2013 kontak` : contacts[0].displayName) || null,
+                contacts,
+            }
+        },
+        {
+            quoted,
+            ...options
+        })
+        }
