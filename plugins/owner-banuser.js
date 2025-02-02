7a43d6d7bf6d9a053ca59f516ac7fb1d9ef61bf8
Ian.xyz <taiwam34@gmail.com>
2025-02-01 21:48:18 -0300

    Update owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -4,12 +4,17 @@ let handler = async (m, { conn, text }) => {
     if (text.includes('@')) {
         who = text.split('@')[1] + '@s.whatsapp.net';
     } else {
-        who = `${text}@s.whatsapp.net`; 
+        who = `${text}@s.whatsapp.net`;
     }
 
     let users = global.db.data.users;
 
+    if (!users[who]) {
+        users[who] = {};
+    }
+
     users[who].banned = true;
+
     conn.reply(m.chat, `✦ *El usuario @${who.split('@')[0]} fue baneado*`, fkontak, { mentions: [who] });
 };
 
