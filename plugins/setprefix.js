f9df8141269cbf509e9453f8674d771b35abd527
Ian.xyz <taiwam34@gmail.com>
2025-02-06 14:09:49 -0300

    PL

--- plugins/setprefix.js
+++ plugins/setprefix.js
@@ -1,19 +1,22 @@
 let handler = async (m, { conn, isRowner }) => {
     const newPrefix = m.text.trim().split(' ').slice(1).join(' ');
-
+  
+   
     if (!newPrefix) {
-        return m.reply('*Proporcione un nuevo prefijo*');
+      return m.reply('*Proporcione un nuevo prefijo por ejemplo (# / .)*.');
     }
+  
+   
+    global.prefix = newPrefix;  
+  
+    
+    m.reply(*El prefijo del bot de cambio a: ${newPrefix}*);
+  
+  
+  };
+  
+ 
+  handler.command = ['setprefix']; 
+  handler.mods = true
 
-    if (!/^[^a-zA-Z0-9]{1,2}$/.test(newPrefix)) {
-        return m.reply('*No se permiten estos caracteres*');
-    }
-
-    global.prefix = newPrefix;
-    m.reply(`*El prefijo del bot se cambió a: ${newPrefix}*`);
-};
-
-handler.command = ['setprefix'];
-handler.mods = true;
-
-export default handler;
+  export default handler;
