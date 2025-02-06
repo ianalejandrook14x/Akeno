0aa57457ea85411699257b6677246a54b8eaff0c
Ian.xyz <taiwam34@gmail.com>
2025-02-06 14:30:58 -0300

    PL

---
+++ plugins/delP.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+
+const handler = async (m, { conn }) => {
+    const pluginsFolder = './plugins';
+    
+    if (!fs.existsSync(pluginsFolder)) {
+        return m.reply('*No se encontró la carpeta de plugins.*');
+    }
+
+    try {
+        const files = fs.readdirSync(pluginsFolder);
+        for (const file of files) {
+            const filePath = path.join(pluginsFolder, file);
+            if (fs.statSync(filePath).isFile()) {
+                fs.unlinkSync(filePath);
+            }
+        }
+        m.reply('*Todos los plugins han sido eliminados.*');
+    } catch (error) {
+        console.error(error);
+        m.reply('*Ocurrió un error al eliminar los plugins.*');
+    }
+};
+
+handler.customPrefix = /^(delP)$/i;
+handler.command = new RegExp;
+
+export default handler;
