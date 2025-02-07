92838c77fb5d09b8bdbdbce20a957c3f53e7503b
Ian.xyz <taiwam34@gmail.com>
2025-02-07 11:26:49 -0300

    PL

--- plugins/Main•allfake.js
+++ plugins/Main•allfake.js
@@ -98,6 +98,6 @@ global.icono = [
 'https://ibb.co/Zz3G9bZ'
 ].getRandom()
 
-global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363318758721861@newsletter", serverMessageId: 100, newsletterName: namechannel, }, externalAdReply: { showAdAttribution: true, title: botname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
+global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363318758721861@newsletter", serverMessageId: 100, newsletterName: namechannel, }, externalAdReply: { showAdAttribution: true, title: botname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: welc, mediaType: 1, renderLargerThumbnail: false }, }, }}
 
 export default handler
