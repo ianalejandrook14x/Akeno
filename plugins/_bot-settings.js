546230c50ec53619a8d279ccf952a9f4dc7add74
Ian.xyz <taiwam34@gmail.com>
2025-02-07 12:34:50 -0300

    PL

--- plugins/_bot-settings.js
+++ plugins/_bot-settings.js
@@ -1,25 +1,40 @@
-const activeBots = new Map(); 
-const handler = async (m, { conn, isOwner }) => {
-    if (!m.isGroup) return; 
+const activeBots = new Map();
+
+const handler = async (m, { conn, isOwner, isAdmin }) => {
+    if (!m.isGroup) return;
 
     const groupId = m.chat;
     const botNumber = conn.user.jid;
 
-    if (activeBots.has(groupId) && activeBots.get(groupId) !== botNumber) {
-        return;
+    if (isAdmin && !activeBots.has(groupId)) {
+        activeBots.set(groupId, botNumber);
     }
 
-    activeBots.set(groupId, botNumber);
+    if (activeBots.has(groupId)) {
+        const activeBot = activeBots.get(groupId);
+        if (activeBot !== botNumber && !(isAdmin && activeBot !== botNumber)) {
+            return;
+        }
+    } else {
+        activeBots.set(groupId, botNumber);
+    }
 
     try {
+        const isCommand = m.text.startsWith('/');
+
+        if (isCommand) {
+            if (isAdmin) {
+            } else {
+            }
+        } else {
+            if (!isAdmin) {
+            } else {
+            }
+        }
     } catch (error) {
         console.error(`Error en el bot ${botNumber}:`, error);
-        activeBots.delete(groupId); 
-    }
-
-    setTimeout(() => {
         activeBots.delete(groupId);
-    }, 60000);
+    }
 };
 
 export default handler;
