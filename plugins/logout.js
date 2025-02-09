aa59a011e8ec4df86442a23b6d2070c8b67dd42a
Ian.xyz <taiwam34@gmail.com>
2025-02-09 12:14:50 -0300

    Update logout.js

--- plugins/logout.js
+++ plugins/logout.js
@@ -15,38 +15,34 @@ const handler = async (m, { conn, text, mentionedJid }) => {
     }
 
     if (!userJid) {
-        return conn.reply(m.chat, "*❀ Responde a un mensaje del usuario o menciona*", m);
+        return conn.reply(m.chat, "*Menciona al usaurio JadiBot o escribe su número*", m);
     }
 
     const userName = userJid.split("@")[0];
     const folderPath = path.join(process.cwd(), "jadi", userName);
-    const subBot = global.conns.find(bot => bot.user?.jid === userJid);
+    const subBotIndex = global.conns.findIndex(bot => bot.user?.jid === userJid);
 
-    if (!subBot) {
-        return conn.reply(m.chat, `*❀ El usuario @${userName} no está conectado.*`, m, { mentions: [userJid] });
+    if (subBotIndex === -1) {
+        return conn.reply(m.chat, `*El usuario @${userName} no está conectado.*`, m, { mentions: [userJid] });
     }
+
+    let subBot = global.conns[subBotIndex];
+    global.conns.splice(subBotIndex, 1);
 
     try {
         if (subBot.ws.readyState === subBot.ws.OPEN) {
-            await subBot.ws.close(DisconnectReason.loggedOut);
+            subBot.ws.close(DisconnectReason.loggedOut);
         }
         subBot.ev.removeAllListeners();
-
-        const index = global.conns.indexOf(subBot);
-        if (index >= 0) {
-            global.conns.splice(index, 1);
-        }
-
-        if (fs.existsSync(folderPath)) {
-            fs.rmdirSync(folderPath, { recursive: true });
-            console.log(`Carpeta de credenciales eliminada para el JadiBot ${userName}.`);
-        }
-
-        await conn.reply(m.chat, `*❀ El JadiBot @${userName} ha sido desconectado.*`, m, { mentions: [userJid] });
     } catch (error) {
         console.error(error);
-        conn.reply(m.chat, "*❀ Hubo un error al intentar desconectar al JadiBot.*", m);
     }
+
+    if (fs.existsSync(folderPath)) {
+        fs.rmdirSync(folderPath, { recursive: true });
+    }
+
+    conn.reply(m.chat, `*El JadiBot @${userName} fue desconectado*`, m, { mentions: [userJid] });
 };
 
 handler.command = ["disconnect"];
