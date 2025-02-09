a06296440f1dbf7f0d2acec7c6651b66e8dfd200
Ian.xyz <taiwam34@gmail.com>
2025-02-09 12:06:30 -0300

    PL

--- plugins/_unbanuser_.js
+++ plugins/_unbanuser_.js
@@ -1,20 +1,30 @@
-let handler = async (m, { conn, text }) => {
+let handler = async (m, { conn, text, mentionedJid }) => {
     let who;
 
-    if (text.includes('@')) {
-        who = text.split('@')[1] + '@s.whatsapp.net'; 
-    } else {
-        who = `${text}@s.whatsapp.net`; 
+    if (mentionedJid?.[0]) {
+        who = mentionedJid[0];
+    } else if (text) {
+        let cleanedNumber = text.replace(/[^0-9]/g, "") + "@s.whatsapp.net";
+        who = cleanedNumber;
     }
 
-    let users = global.db.data.users;
-
-    if (users[who] && users[who].banned) {
-        users[who].banned = false;
-        conn.reply(m.chat, `✦ *El usuario @${who.split('@')[0]} ha sido desbaneado*`, fkontak, { mentions: [who] });
-    } else {
-        conn.reply(m.chat, `✦ *El usuario @${who.split('@')[0]} no está baneado*`, fkontak, { mentions: [who] });
+    if (!who) {
+        return conn.reply(m.chat, "✦ *Menciona el usuario o escribe su número*", m);
     }
+
+    let users = global.db?.data?.users || {};
+
+    if (!users[who]) {
+        return conn.reply(m.chat, `✦ *El usuario @${who.split('@')[0]} no se encuentra en la base de datos.*`, m, { mentions: [who] });
+    }
+
+    if (!users[who].banned) {
+        return conn.reply(m.chat, `✦ *El usuario @${who.split('@')[0]} no se encuentra baneado.*`, m, { mentions: [who] });
+    }
+
+    users[who].banned = false;
+
+    conn.reply(m.chat, `✦ *El usuario @${who.split('@')[0]} ha sido desbaneado.*`, m, { mentions: [who] });
 };
 
 handler.command = ['unbanuser'];
