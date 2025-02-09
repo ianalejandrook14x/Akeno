c3748ceb1704fc3923e7613c47043b722c42671b
Ian.xyz <taiwam34@gmail.com>
2025-02-09 16:56:25 -0300

    SSD

--- plugins/Pokemón.js
+++ plugins/Pokemón.js
@@ -30,5 +30,5 @@ const handler = async (m, { conn }) => {
     await conn.sendMessage(m.chat, { image: { url: mascota.imagen }, caption: mensaje }, { quoted: m });
 };
 
-handler.command = ['pokemon', 'pokemón', 'mascota'];
+handler.command = ['poke', 'mascota'];
 export default handler;
